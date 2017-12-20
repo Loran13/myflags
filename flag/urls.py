062b92a6160b36c16b58ebafdb2008f199185086
Loran13 <loic.hijl@gmail.com>
2017-12-20 20:52:50 +0100

    text

--- flag/urls.py
+++ flag/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.flag_list, name='flag_list'),
+    url(r'^$', views.flag_list, name='index'),
+    url(r'^$', views.flag_list, name='inTheWild'),
+    url(r'^$', views.flag_list, name='learnAboutFlags'),
 ]
