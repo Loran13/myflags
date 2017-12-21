b0dabe7b43f0ee30695fa4424abaf47c3fb9908c
Loran13 <loic.hijl@gmail.com>
2017-12-21 13:59:42 +0100

    text

--- flag/urls.py
+++ flag/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.flag_list, name='flag_list'),
-    url(r'^$', views.flag_list, name='index'),
-    url(r'^$', views.flag_list, name='inTheWild'),
-    url(r'^$', views.flag_list, name='learnAboutFlags'),
+    url(r'^game/$', views.flag_list, name='flag_list'),
+    url(r'^$', views.home, name='home'),
+    url(r'^wild/$', views.wild, name='wild'),
+    url(r'^learn/$', views.learn, name='learn'),
 ]
--- flag/views.py
+++ flag/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from .models import Flag
 
 # Create your views here.
+
+
 def flag_list(request):
-    return render(request, 'flag/flag_list.html')
+    flags = Flag.objects.all()
+    return render(request, 'flag/flag_list.html', {'flags': flags})
+
+def home(request):
+    return render(request, 'flag/index.html')
+
+def wild(request):
+    return render(request, 'flag/inTheWild.html')
+
+def learn(request):
+    return render(request, 'flag/learnAboutFlags.html')
